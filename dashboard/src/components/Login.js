6b0ef15d516ab6fa8ef3382bd0d9fc70068180b3
maviharsh <130988904+maviharsh@users.noreply.github.com>
2025-03-08 15:48:54 +0530

    Update Login.js

--- dashboard/src/components/Login.js
+++ dashboard/src/components/Login.js
@@ -31,7 +31,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/login",
+        "https://tradingapp-backend.onrender.com/login",
         {
           ...inputValue,
         },
